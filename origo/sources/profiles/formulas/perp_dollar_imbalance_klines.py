06af77a2d999ce98b5cad7047138f4847bb5d811
Mikko Kotila <mailme@mikkokotila.com>
2026-09-18 17:25:09 +0300

    refactor(origo): drop dead per-module imbalance bar thresholds

--- origo/sources/profiles/formulas/perp_dollar_imbalance_klines.py
+++ origo/sources/profiles/formulas/perp_dollar_imbalance_klines.py
@@ -4,8 +4,6 @@ import pyarrow as pa
 
 from .generic_bars import imbalance_kline_rows
 
-DOLLAR_IMBALANCE_KLINE_SIZE = 100_000.0
-
 
 def _kline_rows(table: pa.Table) -> pa.Table:
     return imbalance_kline_rows(table)
--- origo/sources/profiles/formulas/spot_dollar_imbalance_klines.py
+++ origo/sources/profiles/formulas/spot_dollar_imbalance_klines.py
@@ -7,8 +7,6 @@ from .generic_bars import imbalance_kline_rows
 DOLLAR_IMBALANCE_KLINES_TABLE_NAME = 'binance_spot_dollar_imbalance_klines'
 RAW_TABLE_NAME = 'binance_daily_spot_trades'
 
-DOLLAR_IMBALANCE_KLINE_SIZE = 100_000.0
-
 
 def _kline_rows(table: pa.Table) -> pa.Table:
     return imbalance_kline_rows(table)
